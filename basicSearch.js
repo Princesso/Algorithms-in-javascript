9d69dd3d47d5ebb6a5abf813f346bbe2c749c714
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-04-08 12:31:27 +0100

    Make search results less tightly coupled with inserted input

--- basicSearch.js
+++ basicSearch.js
@@ -9,9 +9,13 @@ let websitesArray = [
   'payga.com', 
 ]
 
-const searchResults = (searchInput) => {
-  const matches = websitesArray.filter((domainName) => {
+//alternatively, find an api that contains various domain names and implement this, look out for performance
+
+export const searchResults = (searchInput, DB) => {
+  const matches = DB.filter((domainName) => {
     return domainName.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0,5) : matches
 }
+
+// console.log(searchResults('daddy', websitesArray))
