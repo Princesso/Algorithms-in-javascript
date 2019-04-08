740e84d59fdbe114199522d90745542e3dfe8878
Oluebube Princess Egbuna <oluebube.egbuna@andela.com>
2019-04-08 12:37:27 +0100

    Update test suites

--- tests/basicSearch.test.js
+++ tests/basicSearch.test.js
@@ -3,9 +3,23 @@ import { searchResults } from '../basicSearch';
 const mockDB = [
   'xyz.com',
   'abc.com',
-  'paystack.ng'
+  'paystack.ng',
+  'google.com',
+  'alert.com',
+  'fire.com',
+  'alarm.com',
 ]
 
 test('basicSearch', () => {
     expect(searchResults('bube', mockDB)).toEqual([])
+})
+
+test('basicSearch returns 5 results ', () => {
+  expect(searchResults('.com', mockDB).length).toEqual(5)
+})
+
+test('basicSearch returns 5 results ', () => {
+  expect(searchResults(undefined, mockDB)).toEqual([])
+  expect(searchResults(null, mockDB)).toEqual([])
+
 })
